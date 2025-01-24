35c1efc457a6c3de0505ce18e1204015c8ad7197
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-01-25 02:41:53 +0530

    Initial commit!

---
+++ src/main/java/com/romeosarkar10x/java/HelloWorldSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.romeosarkar10x.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldSpringBootApplication.class, args);
+    }
+}
